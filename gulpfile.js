0eab02b05321009ddccc5527d3352d2d0ecbf838
Webb <xwb1990@gmail.com>
2018-08-08 16:51:06 +0800

    优化修改逻辑

--- gulpfile.js
+++ gulpfile.js
@@ -1,23 +1,48 @@
-var gulp = require('gulp');
-var newer = require('gulp-newer');
-var imagemin = require('gulp-imagemin');
-const imageminJpegtran = require('imagemin-jpegtran');
-const imageminPngquant = require('imagemin-pngquant');
+const gulp = require("gulp");
+const newer = require("gulp-newer");
+const imagemin = require("gulp-imagemin");
+const imageminJpegtran = require("imagemin-jpegtran");
+const imageminPngquant = require("imagemin-pngquant");
+const minimist = require("minimist");
+const lodash = require("lodash");
 
-var imgSrc = './image/**';
-var imgDest = './build/image/';
+const options = minimist(process.argv.slice(2));
+const path = require("path");
 
+let { src, dest, json } = options;
+if (!lodash.isEmpty(json)) {
+  const config = require(json);
+  src = config.src;
+  dest = config.dest;
+}
+if (lodash.isEmpty(src)) {
+  console.error("Error src 未配置");
+  return;
+}
+if (lodash.isEmpty(dest)) {
+  console.error("Error dest 未配置");
+  return;
+}
+src = path.resolve(src);
+const watch = src;
+src = path.join(src, "./**/*.{png,jpg,gif,ico}");
+dest = path.resolve(dest);
+
+console.log("src:", src);
+console.log("dest:", dest);
 // Minify any new images
-gulp.task('images', function() {
-
+gulp.task("images", function() {
   // Add the newer pipe to pass through newer images only
-  return gulp.src(imgSrc)
-      .pipe(newer(imgDest))
-      .pipe(imagemin([imageminJpegtran(), imageminPngquant({quality: '65-80'})]))
-      .pipe(gulp.dest(imgDest));
-
+  return gulp
+    .src(src)
+    .pipe(newer(dest))
+    .pipe(
+      imagemin([imageminJpegtran(), imageminPngquant({ quality: "65-80" })], {
+        optimizationLevel: 5
+      })
+    )
+    .pipe(gulp.dest(dest));
 });
-
-gulp.task('default', function() {
-  gulp.watch(imgSrc, ['images']);
+gulp.task("default", function() {
+  gulp.watch(watch, ["images"]);
 });
